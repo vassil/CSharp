a1cec7998ab5967f2d847cf65396a5743e51539c
Stoyan Kirov <stoian.kiroff@gmail.com>
2013-02-02 21:58:08 +0200

    Add last task in Strings

---
+++ 08.StringsAndTextProcessing/03.CheckExpression/03.CheckExpression.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class CheckExpression
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int countBackets = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == '(')
+            {
+                countBackets++;
+            }
+            else if (input[i] == ')')
+            {
+                countBackets--;
+            }
+        }
+
+        if (countBackets==0)
+        {
+            Console.WriteLine("Correct Expression");
+        }
+        else
+        {
+            Console.WriteLine("Inorrect Expression");
+        }
+    }
+}
+
