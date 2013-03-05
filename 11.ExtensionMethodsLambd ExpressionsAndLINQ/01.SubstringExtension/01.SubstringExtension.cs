dcb4ba88684bbfdf24e5eace2eee2caafa23d9c1
Stoyan Kirov <stoian.kiroff@gmail.com>
2013-03-06 01:42:15 +0200

    small fix to task 11.1

--- 11.ExtensionMethodsLambd ExpressionsAndLINQ/01.SubstringExtension/01.SubstringExtension.cs
+++ 11.ExtensionMethodsLambd ExpressionsAndLINQ/01.SubstringExtension/01.SubstringExtension.cs
@@ -5,6 +5,10 @@ public static class SubstringExtension
 {
     public static StringBuilder Substring(this StringBuilder text, int startIndex, int length)
     {
+        if (startIndex<0 || length<=0)
+        {
+            throw new ArgumentOutOfRangeException("Length bigger than 0 and startIndex must be bigger than or euqal to 0.");
+        }
         if (startIndex + length > text.Length)
         {
             throw new ArgumentOutOfRangeException("Index and length must refer to a location within the string.");
@@ -22,6 +26,10 @@ public static class SubstringExtension
 
     public static StringBuilder Substring(this StringBuilder text, int startIndex)
     {
+        if (startIndex < 0)
+        {
+            throw new ArgumentOutOfRangeException("StartIndex must be bigger than or euqal to 0.");
+        }
         if (startIndex > text.Length)
         {
             throw new ArgumentOutOfRangeException("Index and length must refer to a location within the string.");
