d687417d6519bd25936adf17220333e4700eb0a4
Stoyan Kirov <stoian.kiroff@gmail.com>
2013-05-30 22:38:58 +0300

    commit task 12.Stack to DSA lecture 1

--- Data Structures and Algorithms/01.LinearDataStructures/12.StackImplementation/Stack.cs
+++ Data Structures and Algorithms/01.LinearDataStructures/12.StackImplementation/Stack.cs
@@ -1,7 +1,80 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-
-public class Stack
+public class Stack<T> : IEnumerable<T>
 {
-}
+    private T[] stackArray;
+    private int size;
 
+    public Stack()
+    {
+        stackArray = new T[10];
+        size = 0;
+    }
+
+    public int Size
+    {
+        get
+        {
+            return this.size;
+        }
+    }
+
+    public void Push(T value)
+    {
+        if (this.Size == stackArray.Length)
+        {
+            T[] newStack = new T[stackArray.Length * 2];
+            Array.Copy(stackArray, newStack, stackArray.Length);
+            this.stackArray = newStack;
+        }
+
+        stackArray[this.Size] = value;
+        size++;
+    }
+
+    public T Pop()
+    {
+        if (this.Size == 0)
+        {
+            throw new InvalidOperationException("The stack has no elements.");
+        }
+
+        T lastElement = stackArray[this.Size - 1];
+        stackArray[this.Size - 1] = default(T);
+        this.size--;
+
+        return lastElement;
+    }
+
+    public T Peek()
+    {
+        if (this.Size == 0)
+        {
+            throw new InvalidOperationException("The stack has no elements.");
+        }
+
+        T lastElement = stackArray[this.Size - 1];
+
+        return lastElement;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach (var item in stackArray)
+        {
+            if ((dynamic)item == default(T))
+            {
+                break;
+            }
+
+            yield return item;
+        }
+    }
+
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
--- Data Structures and Algorithms/01.LinearDataStructures/12.StackImplementation/StackDemo.cs
+++ Data Structures and Algorithms/01.LinearDataStructures/12.StackImplementation/StackDemo.cs
@@ -4,7 +4,24 @@ public class StackDemo
 {
     static void Main()
     {
+        Stack<int> stack = new Stack<int>();
 
+        stack.Push(5);
+        stack.Push(6);
+        stack.Push(7);
+
+        stack.Pop();
+
+        Console.WriteLine(stack.Peek());
+
+        stack.Push(7);
+        stack.Push(8);
+        stack.Push(9);
+        
+        foreach (var item in stack)
+        {
+            Console.Write("{0} ", item);
+        }
     }
 }
 
